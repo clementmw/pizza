e0f45b1a9a7f4773e8c0a9b1af1c8fe24e350f36
Clement Macharia <clementmacharia62@gmail.com>
2024-02-02 21:24:41 +0300

    2

--- code-challenge1/server/app.py
+++ code-challenge1/server/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from models import db, Restaurant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
